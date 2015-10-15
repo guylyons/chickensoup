b2b4fc842fa1494de8d5db8d19a9f53a8657b22d
Guy Lyons <guylyons2@gmail.com>
2015-10-15 08:03:03 -0400

    Added custom fields

--- front-page.php
+++ front-page.php
@@ -19,11 +19,11 @@ get_header(); ?>
 <!-- landing -->
 <div class="intro">
   <div class="row">
-    <div class="show-for-small-only small-12 columns chicken-logo block">
-      <!-- assets/img/chicken-logo-*.png -->
+    <div class="show-for-small-only small-12 columns block">
+      <img src="<?php the_field("small_logo") ?>" alt="Chicken 300 Small Logo">
     </div>
     <div class="show-for-medium-up medium-12 columns text-center block">
-      <img src="img/chicken-logo-large.png" alt="Chicken 3000 Logo">
+      <img src="<?php the_field("big_logo") ?>" alt="Chicken 300 Big Logo">
     </div>
   </div>
 </div>
@@ -33,10 +33,7 @@ get_header(); ?>
 
 <div class="row">
   <div class="small-12 columns">
-    <h2>Recent Projects</h2>
-    <p>
-      Ornare arcu dui vivamus arcu felis, bibendum ut tristique et, egestas quis. Quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum?
-    </p>
+    <?php the_field("recent_projects"); ?>
   </div>
 </div>
 
@@ -76,7 +73,6 @@ get_header(); ?>
 	    <?php the_content(); ?>
 	<?php endif; ?>
 	  </div>
-
       <?php endwhile; wp_reset_query(); ?>
     </div>
   </div>
@@ -85,83 +81,61 @@ get_header(); ?>
 </div>
 
 
-<!-- Want to see more? -->
-<div class="row">
-  <h1 class="show-for-large-up text-center block">
-    Want to see more of the projects we’ve worked on? Check out our BLAHG.
-  </h1>
-</div>
-
-<!-- buffer-2 -->
-<div class="show-for-medium-up buffer-2 block">
+<?php if(get_field("attention_grabber")): ?>
+  <!-- Want to see more? -->
   <div class="row">
-    <div class="small-12">
-      <blockquote>"Great design is better than bad design."</blockquote>
-      <h6>~ Albert Einstein</h6>
+    <h1 class="show-for-large-up text-center block">
+      <?php the_field("attention_grabber"); ?>
+    </h1>
+  </div>
+<?php endif ?>
+
+<?php if(get_field("quote_1")): ?>
+  <!-- buffer-2 -->
+  <div class="show-for-medium-up buffer-2 block">
+    <div class="row">
+      <div class="small-12">
+	<blockquote>"<?php the_field("quote_1"); ?>"</blockquote>
+      </div>
     </div>
   </div>
-</div>
+<?php endif; ?>
 
-<!-- what the heck -->
-<div class="row">
-  <div class="small-12 columns">
-    <h3>What the heck is Chicken 3000?</h3>
-  </div>
-</div>
-<div class="blue-bg block">
+<?php if(get_field( "big_box_1" ) ): ?>
+  <!-- what the heck -->
   <div class="row">
     <div class="small-12 columns">
-      <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem
-        quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit
-        qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+      <h3>What the heck is Chicken 3000?</h3>
     </div>
   </div>
-</div>
+  <div class="blue-bg block">
+    <div class="row">
+      <div class="small-12 columns">
+	<?php the_field("big_box_1"); ?>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
+
+<?php if(get_field("big_box_2") ): ?>
+  <div class="row what-can-c3k-do-for-you show-for-large-up block">
+    <div class="large-12 columns">
+      <?php the_field("big_box_2"); ?>
+    </div>
+  </div>
+<?php endif; ?>
+
+<?php if(get_field("quote_2") ): ?>
+  <!-- c3k quote block -->
+  <div class="row">
+    <div class="show-for-large-up large-12 columns text-center block">
+      <blockquote><?php the_field("quote_2"); ?></blockquote>
+    </div>
+  </div>
+<?php endif; ?>
+
 
 <!-- triangle of power -->
-
-<div class="row what-can-c3k-do-for-you show-for-large-up block">
-  <h3>What can Chicken 3000 do for YOU?</h3>
-  <div class="show-for-large-up large-4 columns">
-    <ul>
-      <h4>Identity and Branding:</h4>
-      <li>Pretium.</li>
-      <li>Aliquet!</li>
-      <li>Sociis?</li>
-      <li>Rutrum.</li>
-      <li>Ut.</li>
-    </ul>
-  </div>
-  <div class="show-for-large-up large-4 columns">
-    <ul>
-      <h4>Web Design & Programming</h4>
-      <li>Commodo!</li>
-      <li>Enim!</li>
-      <li>Nisl?</li>
-      <li>Massa?</li>
-      <li>Sed.</li>
-    </ul>
-  </div>
-  <div class="show-for-large-up large-4 columns">
-    <ul>
-      <h4>Musicians’ Needs:</h4>
-      <li>Vel.</li>
-      <li>Urna.</li>
-      <li>Mi?</li>
-      <li>Etiam.</li>
-      <li>Id.</li>
-    </ul>
-  </div>
-</div>
-
-<!-- c3k quote block -->
-<div class="row">
-  <div class="show-for-large-up large-12 columns text-center block">
-    <h1>Anything that needs to be looked at can, and should, be designed well.</h1>
-  </div>
-</div>
-
 <div class="row">
   <div class="medium-12 columns">
     <h2>The C3K Triangle of Power</h2>
@@ -169,51 +143,38 @@ get_header(); ?>
 </div>
 
 <div class="row">
+  
   <div class="small-12 large-4 columns triangle-of-power text-center">
-    <img src="assets/img/doug-portrait.jpg">
+    <img src="<?php the_field("headshot_a"); ?>">
     <div class="text-justify">
-      <h2>Doug Von</h2>
-      <h3>Maestro</h3>
-      <a href="">doug(at)chicken3000(dot)com</a>
-      <p>
-        How to make a Doug: take a New York baby raised in New Jersey, blessed with an eye for composition and color, cultivated with a formal education in Graphic Design and Painting, add 10 years of experience to the mix, and boom!</p>
-      <p>Armed with the good sense to keep things simple, and the understanding that information dissemination and visual harmony are conjoined twins, Doug has been able to wrestle many a design challenge to the ground.</p>
+      <?php the_field("bio_a"); ?>
     </div>
   </div>
 
 
   <div class="small-12 large-4 columns triangle-of-power text-center">
-    <img src="assets/img/anna-portrait.jpg">
+    <img src="<?php the_field("headshot_b"); ?>">
     <div class="text-justify">
-      <h2>Anna O.</h2>
-      <h3>Print Jockey</h3>
-      <a href="">anna[at]chicken3000.com</a>
-      <p>Born and raised in the Garden State, schooled in the ways of graphic design and printmaking, genetically self motivated, and currently living in Portland, Maine; Anna is a diehard printmaker at heart, and brings an appreciation of classic graphic
-        design elements to the C3K team. A deep respect for good typography (a regretfully rare thing these days), color, form, line, and composition guides every decision (design and otherwise) that Anna makes.</p>
-
-      <blockquote>“It’s not that I think everything should be a print, it’s just that I know everything would look better if it were printed.”</blockquote>
+      <?php the_field("bio_b"); ?>
     </div>
   </div>
 
 
   <div class="small-12 large-4 columns triangle-of-power text-center">
-    <img src="assets/img/guy-portrait.jpg">
+    <img src="<?php the_field("headshot_c"); ?>">
     <div class="text-justify">
-      <h2>Guy Lyons</h2>
-      <h3>Web Wizard</h3>
-      <a href="">guylyons2[at]gmail.com</a>
-      <p>Since the dawn of time, when blink tags and 'under construction' gifs were fashionable, Guy has been tinkering with the world wide web. A proponent of all things semantic in code and everything irreverent in life, Guy is proud to consider himself
-        "...a total Chicken."</p>
-      <blockquote>""Well, if I called the wrong number, why did you answer the phone?" - James Thurber</blockquote>
+      <?php the_field("bio_c"); ?>
     </div>
   </div>
 </div>
 
-<div class="row">
-  <div class="show-for-large-only large-12 columns text-center block">
-    <h3>At C3K we’re more than just designers: we’re printmakers, painters, illustrators, and thinkers, researchers and resources, and really nice people. So drop us a line about your project and let’s make something truly awesome together!</h3>
+<?php if(get_field("quote_3") ): ?>
+  <div class="row">
+    <div class="show-for-large-only large-12 columns text-center block">
+      <blockquote><?php the_field("quote_3"); ?></blockquote>
+    </div>
   </div>
-</div>
+<?php endif; ?>
 
 </div>
 </div>
