22630dc1fd3f3e5d80789e0ddce8bd4f09eab016
Guy Lyons <guylyons2@gmail.com>
2015-10-16 19:53:43 -0400

    added mason grid

--- archive.php
+++ archive.php
@@ -19,30 +19,66 @@
 get_header(); ?>
 
 <div class="row">
-<!-- Row for main content area -->
-	<div class="small-12 large-8 columns" role="main">
+  <!-- Row for main content area -->
+  TEST
+  <div class="small-12 columns" role="main">
+    <div id="container">
+      <?php if ( have_posts() ) : ?>
 
-	<?php if ( have_posts() ) : ?>
+	<?php /* Start the Loop */ ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+	  
+	  <div class="grid-item">
+	    <?php if (has_post_thumbnail( $post->ID ) ): ?>
+	      <a href="#" data-reveal-id="modal-<?php echo get_the_ID(); ?>">
+		<figure class="text-overlay">
+		  
+		  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+		  <img src="<?php echo $image[0]; ?>">
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+		  <figcaption>
+		    <h3><?php the_title(); ?></h3>
+		    <?php the_excerpt(); ?>
+		    <!-- insert overlay field here -->
+		  </figcaption>
+		</figure>
+	      </a>
+	  </div>
+	  
+	  <div id="modal-<?php echo get_the_ID(); ?>" class="reveal-modal full chicken-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog">
+	    <div class="row">
+	      <div class="medium-4 columns">
+		<?php the_field("column_1"); ?>
+	      </div>
+	      <div class="medium-4 columns">
+		<?php the_field("column_2"); ?>
+	      </div>
+	      <div class="medium-4 columns">
+		
+	      </div>
+	    </div>
+	    <a class="close-reveal-modal" aria-label="Close">&#215;</a>
+	    <?php else: ?>
+	    <?php the_content(); ?>
+	    <?php endif; ?>
+	  </div>
+	<?php endwhile; wp_reset_query(); ?>
+    </div>
+  </div>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+      <?php else : ?>
+  <?php get_template_part( 'content', 'none' ); ?>
 
-	<?php endif; // End have_posts() check. ?>
+      <?php endif; // End have_posts() check. ?>
 
-	<?php /* Display navigation to next/previous pages when applicable */ ?>
-	<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-		<nav id="post-nav">
-			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-		</nav>
-	<?php } ?>
+      <?php /* Display navigation to next/previous pages when applicable */ ?>
+      <?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+	<nav id="post-nav">
+	  <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+	  <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+	</nav>
+      <?php } ?>
 
-	</div>
-	<?php get_sidebar(); ?>
 </div>
+
 <?php get_footer(); ?>
