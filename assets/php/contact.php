ebf69ecf3c3567dd1e1db97f4fe63c8ebdee2302
Guy Lyons <guylyons2@gmail.com>
2015-11-29 13:49:01 -0500

    beautified

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,16 +1,16 @@
 <?php
 /* Set e-mail recipient */
-$myemail  = "doug@chicken3000.com";
+$myemail = 'doug@chicken3000.com';
 
 /* Check all form inputs using check_input function */
-$name = check_input($_POST['name'], "Enter your name");
-$subject  = "You've got Chicken Mail!";
-$email    = check_input($_POST['email']);
-$comments = check_input($_POST['comments'], "Write your comments");
+$name = check_input($_POST['name'], 'Enter your name');
+$subject = "You've got Chicken Mail!";
+$email = check_input($_POST['email']);
+$comments = check_input($_POST['comments'], 'Write your comments');
 
 $success =
 print "<h1>Your message has been sent!</h1> <br>
-   
+
     Name: $name <br>
     E-mail: $email <br>
 
@@ -21,9 +21,8 @@ print "<h1>Your message has been sent!</h1> <br>
 ;
 
 /* If e-mail is not valid show error message */
-if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
-{
-    show_error("This is not a valid e-mail address. Please try again.");
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email)) {
+    show_error('This is not a valid e-mail address. Please try again.');
 }
 
 /* Let's prepare the message for the e-mail */
@@ -43,32 +42,34 @@ End of message
 mail($myemail, $subject, $message);
 
 /* Functions we used */
-function check_input($data, $problem='')
+function check_input($data, $problem = '')
 {
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
-    if ($problem && strlen($data) == 0)
-    {
+    if ($problem && strlen($data) == 0) {
         show_error($problem);
+    } else {
+        echo $success;
     }
-    else { echo $success; }
+
     return $data;
 }
 
 function show_error($myError)
 {
-?>
+    ?>
   <html>
     <body>
 
       <em>Correct the following error:</em>
       <br>
-      <?php echo $myError; ?>
+      <?php echo $myError;
+    ?>
 
     </body>
   </html>
   <?php
   exit();
-  }
+}
   ?>
