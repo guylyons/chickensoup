8f6dbc16121a5ed7126786ce1ac7065661c75a1b
Guy Lyons <guylyons2@gmail.com>
2015-12-11 20:35:48 -0500

    fix chicken.php and add parallax

--- library/chicken.php
+++ library/chicken.php
@@ -1,9 +1,12 @@
 <?php
 
-function my_login_logo() { ?>
+function my_login_logo()
+{
+    ?>
     <style type="text/css">
         .login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/c3k/chicken-logo-small.png);
+            background-image: url(<?php echo get_stylesheet_directory_uri();
+    ?>/assets/images/c3k/chicken-logo-small.png);
             display: block;
             width: 267px;
             height: 361px;
@@ -15,53 +18,20 @@ function my_login_logo() { ?>
           border-top: 5px solid #ef4b26;
         }
     </style>
-<?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
-
-function my_login_logo_url() {
-    return "http://www.chicken3000.com";
-}
-add_filter( 'login_headerurl', 'my_login_logo_url' );
-
-function my_login_logo_url_title() {
-    return 'Chicken 3000';
-}
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
-
-//Custom Theme Settings
-add_action('admin_menu', 'add_gcf_interface');
-
-function add_gcf_interface() {
-    add_options_page('Global Custom Fields', 'Global Custom Fields', '8', 'functions', 'editglobalcustomfields');
-}
-
-function editglobalcustomfields() {
-?>
-  <div class='wrap'>
-    <h2>Global Custom Fields</h2>
-    <form method="post" action="options.php">
-      <?php wp_nonce_field('update-options') ?>
-
-      <p><strong>My Name:</strong><br />
-	<input type="text" name="myname" size="45" value="<?php echo get_option('myname'); ?>" /></p>
-
-	<p><strong>Amazon ID:</strong><br />
-	  <input type="text" name="amazonid" size="45" value="<?php echo get_option('amazonid'); ?>" /></p>
-
-	  <p><strong>Today's Featured Website:</strong><br />
-	    <input type="text" name="todaysite" size="45" value="<?php echo get_option('todaysite'); ?>" /></p>
-
-	    <p><strong>Welcome Text:</strong><br />
-	      <textarea name="welcomemessage" cols="100%" rows="7"><?php echo get_option('welcomemessage'); ?></textarea></p>
-
-	    <p><input type="submit" name="Submit" value="Update Options" /></p>
-
-	    <input type="hidden" name="action" value="update" />
-	    <input type="hidden" name="page_options" value="myname,amazonid,todaysite,welcomemessage" />
-
-    </form>
-  </div>
 <?php
 }
+add_action('login_enqueue_scripts', 'my_login_logo');
+
+function my_login_logo_url()
+{
+    return 'http://www.chicken3000.com';
+}
+add_filter('login_headerurl', 'my_login_logo_url');
+
+function my_login_logo_url_title()
+{
+    return 'Chicken 3000';
+}
+add_filter('login_headertitle', 'my_login_logo_url_title');
 
 ?>
--- library/enqueue-scripts.php
+++ library/enqueue-scripts.php
@@ -29,7 +29,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 
 	// If you'd like to cherry-pick the foundation components you need in your project, head over to Gruntfile.js and see lines 67-88.
 	// It's a good idea to do this, performance-wise. No need to load everything if you're just going to use the grid anyway, you know :)
-	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '5.5.2', true );	
+	wp_enqueue_script( 'foundation', get_template_directory_uri() . '/assets/javascript/foundation.js', array('jquery'), '5.5.2', true );
 
 	// Add the comment-reply library on pages where it is necessary
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
@@ -39,6 +39,7 @@ if ( ! function_exists( 'foundationpress_scripts' ) ) :
 	wp_enqueue_script( 'packery', get_template_directory_uri() . '/assets/javascript/vendor/packery.pkgd.js', array('jquery'), '1.4.3', false );
 	wp_enqueue_script( 'imagesloaded', get_template_directory_uri() . '/assets/javascript/vendor/imagesloaded.pkgd.min.js', array('jquery'), '3.1.8', false );
 	wp_enqueue_script( 'jquery-smooth-scroll', get_template_directory_uri() . '/assets/javascript/vendor/jquery.smooth-scroll.js', array('jquery'), '1.5.6', true);
+  wp_enqueue_script( 'parallax', get_template_directory_uri() . '/assets/javascript/vendor/parallax.min.js', array('jquery'), '2.1.3', true);
 	wp_enqueue_script( 'c3k-custom', get_template_directory_uri() . '/assets/javascript/c3k-custom.js', array('jquery'), '1.0', true );
     }
 
