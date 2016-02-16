16fa0344bc41d011464ac30c2bd29f0c2f22a4e1
Guy Lyons <guylyons2@gmail.com>
2016-02-16 08:29:48 -0500

    infinite scroll support for jetpack

--- functions.php
+++ functions.php
@@ -88,3 +88,9 @@ function cc_mime_types($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'cc_mime_types');
+
+/* support infinite scroll */
+add_theme_support( 'infinite-scroll', array(
+'container' => 'content',
+'footer' => 'page',
+) );
